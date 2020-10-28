f638c61c743a3345d3c146d3b661510bab445ebe
Bruce Wayne <batman@workstation-eu-gregor.eu.local>
2020-10-28 18:45:59 +0100

    simplify the project

--- main.cpp
+++ main.cpp
@@ -2,13 +2,25 @@
 #include <QApplication>
 #include <QLabel>
 #include <QPushButton>
-#include "window.h"
+#include <QLineEdit>
+#include <QVBoxLayout>
 
 int main(int argc, char **argv) {
+    qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
     QApplication app(argc, argv);
-    Window w;
-    w.setWindowState(Qt::WindowFullScreen);
-    w.show();
+
+    QWidget *window = new QWidget;
+
+    QVBoxLayout *layout = new QVBoxLayout();
+    QLineEdit *nameEdit = new QLineEdit("Input line");
+    QPushButton* m_button = new QPushButton("Hello World");
+    m_button->setGeometry(100, 200, 200, 100);
+    layout->addWidget(nameEdit);
+    layout->addWidget(m_button);
+
+    window->setLayout(layout);
+    window->show();
+
     return app.exec();
 
 }

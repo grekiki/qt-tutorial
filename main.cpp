1f415e6adf6445dae7ce443f734b05100244d40a
grekiki <gregor1234567890@gmail.com>
2020-10-29 14:59:31 +0100

    partially works

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "input_field.hpp"
 #include <QtGui>
 #include <QApplication>
 #include <QLabel>
@@ -12,13 +13,21 @@ int main(int argc, char **argv) {
     QWidget *window = new QWidget;
 
     QVBoxLayout *layout = new QVBoxLayout();
-    QLineEdit *nameEdit = new QLineEdit("Input line");
+
+    InputField* i = new InputField;
+    layout-> addWidget(i);
+
+    QLineEdit *nameEdit = new QLineEdit("Dummy");
+
     QPushButton* m_button = new QPushButton("Hello World");
     m_button->setGeometry(100, 200, 200, 100);
+
+    layout->addWidget(i);
     layout->addWidget(nameEdit);
     layout->addWidget(m_button);
 
     window->setLayout(layout);
+    window->setWindowState(Qt::WindowFullScreen);
     window->show();
 
     return app.exec();

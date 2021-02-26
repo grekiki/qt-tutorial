d543b1f0c480db91f7160fd3f29268ecc9e66140
Gregor Kikelj <gregor1234567890@gmail.com>
2021-02-26 01:32:03 +0100

    got the webview

--- main.cpp
+++ main.cpp
@@ -1,33 +1,39 @@
-#include "input_field.hpp"
 #include <QtGui>
 #include <QApplication>
 #include <QLabel>
 #include <QPushButton>
 #include <QLineEdit>
 #include <QVBoxLayout>
+#include <QtCore/QUrl>
+#include <QtCore/QCommandLineOption>
+#include <QtCore/QCommandLineParser>
+#include <QGuiApplication>
+#include <QStyleHints>
+#include <QScreen>
+#include <QQmlApplicationEngine>
+#include <QtQml/QQmlContext>
+#include <QtWebView>
+
 
 int main(int argc, char **argv) {
-    qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
     QApplication app(argc, argv);
+    QtWebView::initialize();
 
     QWidget *window = new QWidget;
 
     QVBoxLayout *layout = new QVBoxLayout();
 
-    InputField* i = new InputField;
-    layout-> addWidget(i);
 
     QLineEdit *nameEdit = new QLineEdit("Dummy");
 
     QPushButton* m_button = new QPushButton("Hello World");
     m_button->setGeometry(100, 200, 200, 100);
 
-    layout->addWidget(i);
     layout->addWidget(nameEdit);
     layout->addWidget(m_button);
 
     window->setLayout(layout);
-    window->setWindowState(Qt::WindowFullScreen);
+    // window->setWindowState(Qt::WindowFullScreen);
     window->show();
 
     return app.exec();
